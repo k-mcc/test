b73aada8e374a5517eb4a3a8bcff58d4c18113c2
Kate McCarthy <k8mccarthy8@icloud.com>
2020-06-16 22:13:17 -0400

    add new file to print kate info

---
+++ kate_learns_git.py
@@ -0,0 +1,8 @@
+while(True):
+    print("Kate's gotta learn it!")
+    print("GOTTA INSTALL PYTHON 3\n")
+    try:
+        abs("kate knows it")
+    except:
+        # she doesn't
+        continue
